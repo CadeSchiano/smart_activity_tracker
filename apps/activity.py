5ceccd07faae550a14ddac71843d9d0555c1d776
“Cade <cschian@bgsu.edu>
2026-01-29 14:33:24 -0500

    Add validation and behavior to Activity model

--- apps/activity.py
+++ apps/activity.py
@@ -1,16 +1,17 @@
 class Activity:
-    def __init__(self, title, category, location, time):
+    def __init__(self, title: str, category: str, location: str, time: str):
         self.title = title
         self.category = category
         self.location = location
         self.time = time
 
+        if not all([self.title, self.category, self.location, self.time]):
+            raise ValueError("All activity fields must be provided.")
+
     def to_dict(self) -> dict:
         return {
-            'title': self.title,
-            'category': self.category,
-            'location': self.location,
-            'time': self.time,
+            "title": self.title,
+            "category": self.category,
+            "location": self.location,
+            "time": self.time
         }
-
-   
