a038670c43a8d6f4e762031007652a5829fbe8be
“Cade <cschian@bgsu.edu>
2026-03-25 14:56:10 -0400

    Fix error

--- apps/auth.py
+++ apps/auth.py
@@ -1,7 +1,10 @@
 from passlib.context import CryptContext
 from passlib.exc import UnknownHashError
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["pbkdf2_sha256", "bcrypt"],
+    deprecated="auto",
+)
 
 
 def hash_password(password: str):
